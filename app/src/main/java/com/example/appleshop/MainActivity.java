d0729fd8ed99aaa6f31c09d6cf80e2b845c6c029
alexkomanov <akomanov88@gmail.com>
2019-10-14 12:42:45 +0300

    Add Whole Project updated

--- app/src/main/java/com/example/appleshop/MainActivity.java
+++ app/src/main/java/com/example/appleshop/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     String goodsName;
     double price;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
